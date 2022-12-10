30b8c0cd74db69cff2c33590821375ee24fcbb54
dmitry <dmitry.skor99@gmail.com>
2022-12-10 18:09:07 +0300

    remove button on mainActivity

--- app/src/main/java/ru/dmitryskor/movies/MainActivity.kt
+++ app/src/main/java/ru/dmitryskor/movies/MainActivity.kt
@@ -9,14 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonChangeMode = findViewById<Button>(R.id.button_change_night_mode)
-        buttonChangeMode.setOnClickListener {
-            val isNight = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
-            if (isNight) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }
-        }
     }
 }
