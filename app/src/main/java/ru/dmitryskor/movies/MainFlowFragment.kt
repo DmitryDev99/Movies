a427f3ccd2a68814d1f94b30c78dc5a009ceb9bd
dmitry <dmitry.skor99@gmail.com>
2022-12-14 01:33:04 +0300

    переезд основного фрагмента

--- app/src/main/java/ru/dmitryskor/movies/MainFlowFragment.kt
+++ app/src/main/java/ru/dmitryskor/movies/MainFlowFragment.kt
@@ -1,14 +1,15 @@
 package ru.dmitryskor.movies
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.navigation.NavController
-import androidx.navigation.NavGraph
+import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.dmitryskor.movies.core.navigation.BaseFlowFragment
-import ru.dmitryskor.movies.core.navigation.navigateSafely
+import ru.dmitryskor.movies.databinding.FlowFragmentMainBinding
 
 /**
  * Created by Dmitry Skorodumov on 10.12.2022
@@ -18,34 +19,58 @@ class MainFlowFragment : BaseFlowFragment(
     R.layout.flow_fragment_main,
     R.id.main_nav_host_fragment_container
 ) {
+
+    private var _binding: FlowFragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FlowFragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val bottomNavMenu = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        initBottomNavView()
+    }
+
+    private fun setDefaultSelectedItem(bottomNavMenu: BottomNavigationView, itemId: Int) {
+        bottomNavMenu.selectedItemId = itemId
+    }
+
+    private fun initBottomNavView() {
         val fragContainer = childFragmentManager.findFragmentById(R.id.main_nav_host_fragment_container) as NavHostFragment
         val navController = fragContainer.navController
-        val navGraphMovies = navController.navInflater.inflate(R.navigation.movies_graph)
-        val navGraphApps = navController.navInflater.inflate(R.navigation.apps_graph)
-
-        setDefaultSelectedItem(bottomNavMenu, navController, navGraphMovies, R.id.movies_item_main_menu)
-
-        bottomNavMenu.setOnItemSelectedListener {
+        binding.bottomNavigation.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.isVisible = getFragmentsWithBottomNav().any{ it == destination.id }
+        }
+        binding.bottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.movies_item_main_menu -> {
-                    navController.graph = navGraphMovies
-//                    val action = MainFlowFragmentDirections.actionMainFlowFragmentToMoviesGraph()
-//                    findNavController().navigateSafely(action)
+                R.id.moviesFlowFragment -> {
+                    navController.graph = navController.navInflater.inflate(R.navigation.movies_graph)
                 }
-                R.id.list_apps_item_main_menu -> {
-                    navController.graph = navGraphApps
-//                    val action = MainFlowFragmentDirections.actionMainFlowFragmentToAppsGraph()
-//                    findNavController().navigateSafely(action)
+                R.id.listAppsFlowFragment -> {
+                    navController.graph = navController.navInflater.inflate(R.navigation.apps_graph)
                 }
             }
             return@setOnItemSelectedListener true
         }
+        setDefaultSelectedItem(binding.bottomNavigation, R.id.moviesFlowFragment)
     }
 
-    private fun setDefaultSelectedItem(bottomNavMenu: BottomNavigationView, navController: NavController, navGraph: NavGraph, itemId: Int) {
-        bottomNavMenu.selectedItemId = itemId
-        navController.graph = navGraph
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
+    companion object {
+        private fun getFragmentsWithBottomNav() = listOf(
+            R.id.moviesFlowFragment,
+            R.id.listAppsFlowFragment,
+        )
+    }
+
 }
